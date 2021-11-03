957f2cc879f5989c27a0ecd701b5ce89472808e8
fernaldyl <16520098@std.stei.itb.ac.id>
2021-11-03 22:46:34 +0700

    updated main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("123");
+int main(){
+
+    boolean running;
+    // kapasitas tas int
+    // ability (speed boost , return to sender) list statis boolean
+
+    // main menu
+
+    while (running){
+
+        // input
+            // move
+                // waktu
+            // pick_up
+            // drop_off
+            // map
+            // to_do
+            // in_progress
+            // buy (gadget)
+            // inventory (gadget)
+            // help
+            // save game (bonus)
+            // return (bonus)
+            // endgame -> running = false
+
+    }
+
     return 0;
 }
