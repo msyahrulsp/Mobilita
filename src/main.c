8c77ce0b7093b84d338afb34017dc9c6d8d8dca4
fernaldyl <16520098@std.stei.itb.ac.id>
2021-11-08 20:47:17 +0700

    edited main

--- src/main.c
+++ src/main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "boolean.h"
+#include "LinkedList\list_linked.h"
+#include "ListDinamis\listdin.h"
+#include "ListStatis\listStatik.h"
+#include "Matriks\matrix.h"
+#include "MesinKardanMesinKata\wordmachine.h"
+#include "PColor\pcolor.h"
+#include "Queue\queue.h"
+#include "Stack\stack.h"
 
 int main(){
 
